2fb391f8c59a5e9e337cd799234c536482e82af3
Antonio Cabral <gutocb7@gmail.com>
2023-10-27 13:41:32 -0300

    Update api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -4,12 +4,12 @@ import { Dish } from '../interfaces/Dish';
 import { Order } from '../interfaces/Order';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3004',
+  baseURL: 'http://localhost:5048',
 });
 
 export const createOrder = async (orderData: Order) => {
   try {
-    const response = await api.post('/orders', orderData);
+    const response = await api.post('/api/v1/orders', orderData);
     return response.data;
   } catch (error) {
     throw error;
@@ -18,7 +18,7 @@ export const createOrder = async (orderData: Order) => {
 
 export const fetchOrders = async () => {
   try {
-    const response = await api.get('/orders');
+    const response = await api.get('/api/v1/orders');
     return response.data;
   } catch (error) {
     throw error;
@@ -27,7 +27,7 @@ export const fetchOrders = async () => {
 
 export const fetchMenuItems = async (): Promise<Dish[]> => {
     try {
-      const response = await api.get('/menu');
+      const response = await api.get('/api/v1/dishes');
       return response.data;
     } catch (error) {
       throw error;
